ab6a1b8cb085ffef7f513f4eb70691842aa0a41d
Lucero Valdez <lvaldezd.isc91@gmail.com>
2024-01-04 15:43:15 -0600

    add new code sprin boot project 2

--- srv-products/src/main/java/com/lvd/app/products/SrvProductsApplication.java
+++ srv-products/src/main/java/com/lvd/app/products/SrvProductsApplication.java
@@ -3,6 +3,10 @@ package com.lvd.app.products;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// @SpringBootApplication: Anotación que sirve para habilitar tres funciones:
+// @EnableAutoConfiguration: habilita el mecanismo de configuración automática de Spring Boot
+// @ComponentScan: habilite @Component el escaneo en el paquete donde se encuentra la aplicación 
+// @Configuration: permite registrar beans adicionales en el contexto o importar clases de configuración adicionales
 @SpringBootApplication
 public class SrvProductsApplication {
 
