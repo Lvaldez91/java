ae5a30222ce4edbc21067584b03e0c7d4d6db709
Lucero Valdez <lvaldezd.isc91@gmail.com>
2024-01-04 13:44:14 -0600

    add new code sprin boot project

---
+++ srv-products/src/main/java/com/lvd/app/products/SrvProductsApplication.java
@@ -0,0 +1,13 @@
+package com.lvd.app.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrvProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrvProductsApplication.class, args);
+	}
+
+}
