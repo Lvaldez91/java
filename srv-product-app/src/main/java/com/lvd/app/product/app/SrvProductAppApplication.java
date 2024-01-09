371564f4596a8c7299c68b486356517c36df2edb
Lucero Valdez <lvaldezd.isc91@gmail.com>
2024-01-08 20:52:20 -0600

    add new code sprin boot project

--- srv-product-app/src/main/java/com/lvd/app/product/app/SrvProductAppApplication.java
+++ srv-product-app/src/main/java/com/lvd/app/product/app/SrvProductAppApplication.java
@@ -2,8 +2,11 @@ package com.lvd.app.product.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class SrvProductAppApplication {
 
 	public static void main(String[] args) {
